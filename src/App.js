0fb7d7b0c88b3e5d97331c5350367c5ad1c0491f
AustinPrabu <lebiaustin333@gmail.com>
2024-10-23 00:59:56 +0530

    pdf convertion changes

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import PdfToHtmlToXml from './components/pdf-to-html-to-xml';
+
+function App() {
+  return (
+    <div className="App">
+      <PdfToHtmlToXml/>
+    </div>
+  );
+}
+
+export default App;
